b6b2499d8205ccbc68276f8dabef5f239ab7bc6b
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2022-11-13 05:44:11 +0100

    testing...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import SignIn from './components/signIn'
 import './App.css';
+import SignUp from './components/signUp';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <SignIn />
+      <SignUp />
     </div>
   );
 }
