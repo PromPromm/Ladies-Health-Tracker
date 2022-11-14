b62104c3e274d913c3cde0ce10ddeb95535231aa
Oluwatoyin <oluwatoyinjolaoso001@gmail.com>
2022-11-14 13:03:53 +0100

    Added log in page

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,8 +1,32 @@
+import React from "react";
+import "../index.css";
+//import { Link } from "react-router-dom";
 const SignIn = () => {
   return (
-    <div>
-    <h1>Sign In</h1>
-    </div>
-  )
-}
-export default SignIn
+    <>
+      <div className="wrapper">
+        <header>Patient Portal</header>
+        <section className = "signin-info">
+          <p className = "header-p">Log in</p>
+          <section>
+            <form>
+              <label>Email Address</label><br />
+              <input type="email" placeholder ="Email" /><br />
+
+              <label>Password</label><br />
+              <input type="password" placeholder = "********" />
+            </form>
+            {/* <Link to="#"> Forgot Password?</Link> */}
+            <button type="submit">Log in</button>
+          </section>
+          <aside>
+            <hr />
+            <p className = "new-here">I am new here</p>
+            <p className = "create">Create Account</p>
+          </aside>
+        </section>
+      </div>
+    </>
+  );
+};
+export default SignIn;
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,8 +1,28 @@
+import React from "react";
+//import { Link } from 'react-router-dom'
 const SignUp = () => {
   return (
-    <div>
-    <h1>SignUp</h1>
-    </div>
-  )
-}
-export default SignUp
+    <>
+      <header>Patient Portal</header>
+      <section>
+        <p>Log In</p>
+        <section>
+          <form>
+            <label for="fname">First name:</label>
+            <input type="text" id="fname" name="fname" />
+
+            <label for="lname">Last name:</label>
+            <input type="text" id="lname" name="lname" />
+
+            <label for="email">Email:</label>
+            <input type="text" id="email" name="email" />
+
+            <label for="password">Password:</label>
+            <input type="text" id="passwaord" name="password" />
+          </form>
+        </section>
+      </section>
+    </>
+  );
+};
+export default SignUp;
